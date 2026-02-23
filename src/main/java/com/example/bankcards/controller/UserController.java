78a090dc30ff7709bdeec76b887d78fb28e4e15c
dinya1234 <ulianagaladzeva@gmail.com>
2026-02-23 22:07:33 +0300

    созданы сущность User и для него UserController,UserService и UserRepository

---
+++ src/main/java/com/example/bankcards/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.example.bankcards.controller;
+
+import com.example.bankcards.entity.User;
+import com.example.bankcards.service.UserService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+}
